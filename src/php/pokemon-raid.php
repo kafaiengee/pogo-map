3d0548d04461ff90d064c7eb645f867107fa0e32
Kafai Engee <kafaiengee@weborama.nl>
2018-05-09 16:23:57 +0200

    added pokemonphp for raids

---
+++ src/php/pokemon-raid.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once('common.php');
+
+$query = 'SELECT * FROM `pokemons` WHERE `raid_lvl` > 0 ORDER BY `pokemons`.`raid_lvl` ASC';
+
+$result = mysqli_query($dblink, $query) or die(mysqli_error($dblink));
+$num_rows = mysqli_num_rows($result);
+
+while ( $row = mysqli_fetch_array($result, MYSQLI_ASSOC) ) {
+  $pokemons = array();
+  $pokemon = array();
+  $pokemon_id = '';
+  $raid_lvl_array = array();
+  $raid_lvl = '';
+  
+  foreach ($row as $pokemon_key => $pokemon_value) {
+    $pokemon_key = trim((string)$pokemon_key);
+    $pokemon_value = trim((string)$pokemon_value);
+
+    if ($pokemon_key == 'raid_lvl') {
+      $raid_lvl = $pokemon_value;
+    } else if ($pokemon_key == 'id') {
+      $pokemon_id = $pokemon_value;
+    } else {
+      $pokemon[$pokemon_key] = $pokemon_value;
+    }
+  }
+  
+  $data[$raid_lvl][$pokemon_id] = $pokemon;
+}
+
+print_r(json_encode(utf8ize($data), JSON_FORCE_OBJECT));
+// print_r($data);
+
+json_last_error();
